578c6a6ef2665f0e910be9db16c49802370de710
Theboiblazin <Theboiblazin2026@gmail.com>
2026-02-09 21:41:42 -0500

    feat: Upgrade Damage Inspector (AI + DB) and Fix News Feed

--- pocket_academy/vite.config.js
+++ pocket_academy/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Fix for Vercel blank page
   server: {
     host: true, // Exposes the server to the local network
   }
